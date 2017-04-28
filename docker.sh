a5d0fc9791cef7e2e6171842525e3bd53eba0787
karthikaws <karthikeshwar29@gmail.com>
2017-04-28 16:07:18 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 docker build -t="httpd_server" .
-docker run -itd --name mount_git -v /root/gitclone:/usr/local/apache2/htdocs httpd_server
+docker run -itd --name mount_git -v /root/git_clone:/usr/local/apache2/htdocs httpd_server
