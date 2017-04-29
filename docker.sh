9d60b33a0cd86125be9cad5f39e20f648aeb4a64
karthikaws <karthikeshwar29@gmail.com>
2017-04-28 21:48:37 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 docker build -t="httpd_server" .
-docker run -itd --name mount_git -v /root/DevOps:/usr/local/apache2/htdocs -p 80:80 httpd_server /bin/bash
+docker run -itd --name mount_git -v /root/DevOps:/usr/local/apache2/htdocs -p 80:80 httpd_server 
